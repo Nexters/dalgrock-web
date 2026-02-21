00bccf681ac1321fb5072f3f01404502999b60ff
hjy0951 <hjy0951@gmail.com>
2026-02-21 19:49:52 +0900

    design: 주차별 기록 섹션 내 불필요한 border 제거

--- src/pages/records/_components/weekly-section-layout.tsx
+++ src/pages/records/_components/weekly-section-layout.tsx
@@ -10,7 +10,6 @@ export function WeeklySectionLayout({ children }: WeeklySectionLayoutProps) {
       style={{
         background: '#1C1D2399',
         boxShadow: 'inset 0px 0px 20px 0px #FFFFFF0A',
-        border: '1px solid',
         borderRadius: '16px',
         borderImageSource:
           'linear-gradient(142.18deg, rgba(255, 255, 255, 0.036) 14.51%, rgba(255, 255, 255, 0.012) 50.05%, rgba(255, 255, 255, 0.036) 85.94%)',
