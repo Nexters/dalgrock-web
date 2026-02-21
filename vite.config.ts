fcf78de026a32f321cac06c27c57c80a7a795028
hjy0951 <hjy0951@gmail.com>
2026-02-22 02:02:49 +0900

    chore: https 환경 설정 및 dev.pliview.kr 도메인 세팅 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import mkcert from 'vite-plugin-mkcert'
 import path from 'path'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    react(),
+    tailwindcss(),
+    mode === 'https' && mkcert({ hosts: ['localhost', 'dev.pliview.kr'] })
+  ],
+  server: {
+    host: true,
+    allowedHosts: ['dev.pliview.kr']
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
     }
   }
-})
+}))
