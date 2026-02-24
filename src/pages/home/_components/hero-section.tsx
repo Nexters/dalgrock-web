4c5bfc1aed33bd8d2543676137d417cfeac92eef
JKyEun <j56237@naver.com>
2026-02-24 23:14:35 +0900

    feat: 히어로 섹션 컴포넌트 스타일 여백 수정

--- src/pages/home/_components/hero-section.tsx
+++ src/pages/home/_components/hero-section.tsx
@@ -1,6 +1,6 @@
 function HeroSection() {
   return (
-    <div className="px-5 pt-3">
+    <div className="px-5 pt-6">
       <h1 className="text-[22px] font-bold leading-[32px] tracking-[-0.25px] text-white">
         음악으로 오늘을 기록해보세요!
       </h1>
