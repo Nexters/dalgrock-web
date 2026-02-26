42ef6b681242c7ae7e1bc63f3675d66ab02de7d0
hjy0951 <hjy0951@gmail.com>
2026-02-27 01:00:05 +0900

    fix: toast가 가운데 뜨지 않는 현상 수정

--- src/App.tsx
+++ src/App.tsx
@@ -9,16 +9,19 @@ function App() {
     <div className="mx-auto min-h-dvh max-w-[var(--max-viewport)] bg-gray-600">
       <RouterProvider router={router} />
       <Toaster
-        position="top-center"
+        position="bottom-center"
+        style={{ bottom: '100px' }}
         toastOptions={{
           style: {
+            width: 'fit-content',
+            left: 0,
+            right: 0,
+            margin: '0 auto',
             background: '#383B45',
             color: '#FFFFFF',
             borderRadius: '30px',
             border: 'none',
             padding: '14px 20px',
-            width: 'fit-content',
-            margin: '0 auto',
             fontSize: '15px',
             fontWeight: '500',
             gap: '10px'
